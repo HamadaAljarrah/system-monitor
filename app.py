2db4d86546c687c1903fbdb2d357357b0e30361d
MarcusOkodugha <100699584+MarcusOkodugha@users.noreply.github.com>
2024-02-05 11:27:27 +0100

    added cpu energy

--- app.py
+++ app.py
@@ -9,18 +9,36 @@ app = Flask(__name__)
 
 
 
+# def get_nvidia_gpu_power_usage():
+#     """Get the current power usage for NVIDIA GPUs using nvidia-smi."""
+#     try:
+#         nvidia_smi_output = subprocess.run(['nvidia-smi', '--query-gpu=power.draw', '--format=csv,noheader,nounits'], stdout=subprocess.PIPE, text=True)
+#         power_usage_str = nvidia_smi_output.stdout.strip()  # Power usage in watts
+#         # In systems with multiple GPUs, this will return the power usage of the first one. Adjust accordingly.
+#         power_usage = float(power_usage_str.split('\n')[0])  # Convert first GPU's power usage to float
+#         return f"{power_usage} W"
+#     except Exception as e:
+#         return f"Failed to read GPU power usage: {str(e)}"
+
 def get_nvidia_gpu_power_usage():
     """Get the current power usage for NVIDIA GPUs using nvidia-smi."""
     try:
-        nvidia_smi_output = subprocess.run(['nvidia-smi', '--query-gpu=power.draw', '--format=csv,noheader,nounits'], stdout=subprocess.PIPE, text=True)
+        nvidia_smi_output = subprocess.run(
+            ['nvidia-smi', '--query-gpu=power.draw', '--format=csv,noheader,nounits'],
+            stdout=subprocess.PIPE,
+            text=True
+        )
         power_usage_str = nvidia_smi_output.stdout.strip()  # Power usage in watts
+        
+        # Handle [N/A] response
+        if '[N/A]' in power_usage_str:
+            return "Power usage not available for this GPU."
+        
         # In systems with multiple GPUs, this will return the power usage of the first one. Adjust accordingly.
         power_usage = float(power_usage_str.split('\n')[0])  # Convert first GPU's power usage to float
         return f"{power_usage} W"
     except Exception as e:
         return f"Failed to read GPU power usage: {str(e)}"
-
-# Example usage
 
 
 
@@ -46,19 +64,23 @@ def health():
 def usage():
     """Endpoint to get current CPU and GPU usage."""
     # Example usage
-    energy = get_nvidia_gpu_power_usage()
+    cpu_energy = psutil.cpu_freq()
     cpu_usage = psutil.cpu_percent(1)
+   
     gpu_usage = get_gpu_usage()
+    gpu_energy = get_nvidia_gpu_power_usage()
+   
     memory_usage = psutil.virtual_memory()
     disk_usage = psutil.disk_usage('/')
     network_usage = psutil.net_io_counters()
     return {
         "CPU Usage": f"{cpu_usage}%",
+        "CPU Energy": f"{cpu_energy}",
         "GPU Usage": gpu_usage,
         "Memory Usage": f"{memory_usage.percent}%",
         "Disk Usage": f"{disk_usage.used} bytes used, {disk_usage.free} bytes free, total {disk_usage.total} bytes",
         "Network Usage": f"{network_usage.bytes_sent} bytes sent, {network_usage.bytes_recv} bytes received",
-        "Energy": energy,
+        "GPU Energy": gpu_energy,
 
     }
 
