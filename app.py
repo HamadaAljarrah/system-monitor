a945aa246dfde8b3c3c310a773e0abdb380cbbad
PaulW02 <pslwi@kth.se>
2024-02-22 13:55:21 +0100

    print namespaces

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
 from kubernetes import client, config
-import subprocess
 # Load Kubernetes configuration from default location
-#config.load_kube_config()
+config.load_kube_config()
 
 # Create Kubernetes client
-#api_instance = client.CoreV1Api()
+api_instance = client.CoreV1Api()
 
-
-def get_all_namespaces():
-    try:
-        # Execute the kubectl command to get namespaces
-        result = subprocess.check_output(['kubectl', 'get', 'namespaces'])
-        # Convert the output to a string and split it into lines
-        namespaces = result.decode('utf-8').split('\n')
-        # Print each namespace
-        for namespace in namespaces[1:]:  # Skip the header line
-            if namespace:  # Skip empty lines
-                print("Namespace:", namespace.split()[0])
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to retrieve namespaces: {e}")
-
-"""def print_all_namespaces():
+def print_all_namespaces():
     # Get all namespaces in the Kubernetes cluster
     try:
         namespaces = api_instance.list_namespace().items
@@ -28,7 +13,6 @@ def get_all_namespaces():
             print("TEST:", namespace.metadata.name)
     except Exception as e:
         print(f"Failed to retrieve namespaces: {str(e)}")
-        """
 
 if __name__ == "__main__":
-    get_all_namespaces()
+    print_all_namespaces()
